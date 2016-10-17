89035656548fa6e2f8c9dfc236b282230a2f5af9
Kathleen <kayjsmyth@gmail.com>
2016-10-17 09:35:02 -0400

    generated devise views

--- spec/models/cheer_spec.rb
+++ spec/models/cheer_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Cheer, type: :model do
-   before(:each) do
-    	@cheer = Cheer.first
-    	@usr = User.first
-    	@class = MyClass.first
-    	@cheer.user = @user
-    	@cheer.class = @class
+     before(:each) do
+    @user = User.create(first_name: "Andrew", email: "andrew.dasilva@lol.com", password: "probablyBatman", password_confirmation: "probablyBatman")
+    @user2 = User.create(first_name: "Erin", email: "erin.noodles@wat1.com", password: "amidone", password_confirmation: "amidone")
+    @course = Course.create(user_id: @user.id, course_title: "How to cook spaghetti", topic: "Cooking")
+    @some_task = Task.create(course_id: @course.id)
   end
 
   it "belongs to one class" do
