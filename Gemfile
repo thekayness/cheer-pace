8d0e090af9ab961d7fdf998fb8bd23f536d1ba0f
Kathleen <kayjsmyth@gmail.com>
2016-10-19 11:15:37 -0400

    checking gitignore

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
+gem 'omniauth-github'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
@@ -41,7 +42,6 @@ group :development, :test do
   gem "factory_girl_rails"
   gem "simplecov"
   gem "database_cleaner"
-  gem "sqlite3"
   gem "pry"
   gem "guard-rspec", require: false
   gem "thin"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016143652) do
+ActiveRecord::Schema.define(version: 20161019145703) do
 
   create_table "cheers", force: :cascade do |t|
     t.string   "content"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20161016143652) do
     t.string   "encrypted_password", default: "", null: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
